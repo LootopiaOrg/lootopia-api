8ca8203429d1b0e7349817a4f68b5f038707ed46
BenkhaddaMd <benkhada111@gmail.com>
2025-05-27 23:31:31 +0200

    feature/integrate-participation

--- src/main/java/com/lootopiaApi/repository/HuntRepository.java
+++ src/main/java/com/lootopiaApi/repository/HuntRepository.java
@@ -4,9 +4,13 @@ import com.lootopiaApi.model.entity.Hunt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @Repository
 public interface HuntRepository extends JpaRepository<Hunt, Long> {
     List<Hunt> findByOrganizer_Id(Long id);
+    Page<Hunt> findByAccessModeAndEndDateAfter(String accessMode, LocalDateTime now, Pageable pageable);
 }
---
+++ src/main/java/com/lootopiaApi/repository/ParticipationRepository.java
@@ -0,0 +1,12 @@
+package com.lootopiaApi.repository;
+
+import com.lootopiaApi.model.entity.Participation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface ParticipationRepository extends JpaRepository<Participation, Long> {
+
+    Optional<Participation> findByUserIdAndHuntId(Long playerId, Long huntId);
+    List<Participation> findAllByUserId(Long userId);
+}
